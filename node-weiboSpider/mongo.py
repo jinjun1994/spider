01d848414f8ddf5d8f5aa320cf8aea7d0b7db5b9
jinjun1994 <jinjun199403@163.com>
2020-03-01 23:03:24 +0800

    mongod测试文件修改

--- node-weiboSpider/mongo.py
+++ node-weiboSpider/mongo.py
@@ -1,10 +1,11 @@
 import pymongo
 # https://api.mongodb.com/python/current/tutorial.html
 # mongo -u jinjun -p jj044019 149.248.5.21/admin
-mongodbUri = 'mongodb://jinjun:jj044019@149.248.5.21/admin'
+# mongodbUri = 'mongodb://jinjun:jj044019@149.248.5.21/admin'
 
-client = pymongo.MongoClient(mongodbUri)
-db = client.spider
+# client = pymongo.MongoClient(mongodbUri)
+client = pymongo.MongoClient()
+db = client.weibo
 print(db)
-a = db.test.find_one()
+a = db.user.find_one()
 print(a)
