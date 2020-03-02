41f079e01159ac88faf4ca66d32ee94ea6f0e2ed
jinjun1994 <jinjun199403@163.com>
2020-03-02 20:30:47 +0800

    vue-跨域配置

--- vue-spider/vue.config.js
+++ vue-spider/vue.config.js
@@ -8,7 +8,14 @@ module.exports = {
     config.module.rule('eslint').use('eslint-loader').options({
       fix: true
     })
+    config
+    .plugin('html')
+    .tap(args => {
+      args[0].title = '集智阁'
+      return args
+    })
   },
+
   // configureWebpack: {
   //   devtool: 'source-map',
   //   // plugins: [
@@ -20,6 +27,17 @@ module.exports = {
   //   }
   // },
   devServer: {
-    proxy: process.env.VUE_APP_PROXY || 'http://localhost:7001/',
+    // proxy: process.env.VUE_APP_PROXY || 'http://localhost:7001/',
+       // disableHostCheck: true,
+      port: 8888,
+      open: true,
+      proxy: {
+        '/api': {
+          target: 'http://localhost:7001/',
+          pathRewrite: {
+            '^/api': '/'
+          }
+        },
+      }
   },
 };
