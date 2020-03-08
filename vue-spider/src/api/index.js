d31cfb7fc4f6c674d10fecb9205cda3089f0b32c
jinjun1994 <jinjun199403@163.com>
2020-03-08 12:39:09 +0800

    vue-csrf

--- vue-spider/src/api/index.js
+++ vue-spider/src/api/index.js
@@ -1,5 +1,16 @@
 import axios from 'axios';
-axios.defaults.headers['x-csrf-token'] = 'iKR4tho-ZwEA4zDWvk-4twH4'; // attach cookie to axiosInstance for future requests
+function getCookie(cname) {
+  const name = cname + '=';
+  const ca = document.cookie.split(';');
+  for (let i = 0; i < ca.length; i++) {
+    const c = ca[i].trim();
+    if (c.indexOf(name) === 0) return c.substring(name.length, c.length);
+  }
+  return '';
+}
+const csrfToken = getCookie('csrfToken');
+axios.defaults.headers['x-csrf-token'] = csrfToken; // attach cookie to axiosInstance for future requests
+// axios.defaults.headers['x-csrf-token'] = 'iKR4tho-ZwEA4zDWvk-4twH4'; // attach cookie to axiosInstance for future requests
 
 export async function fetchPeople(params) {
   const list = await axios.get('/api/user', { params });
