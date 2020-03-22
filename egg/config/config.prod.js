72d058cf9ea0c4ccf5888fac15eba2d7a97cee3a
jinjun1994 <jinjun199403@163.com>
2020-03-22 12:41:36 +0800

    egg-mongod

--- egg/config/config.prod.js
+++ egg/config/config.prod.js
@@ -13,8 +13,8 @@ module.exports = appInfo => {
   const config = exports = {
 
     mongoose: {
-      url: process.env.SPIDER_MONGODB || 'mongodb://127.0.0.1/weibo',
-      // url: 'mongodb://localhost/weibo',
+      // url: process.env.SPIDER_MONGODB || 'mongodb://127.0.0.1/weibo',
+      url: 'mongodb://localhost/weibo',
       options: {
         // auth: { authSource: 'admin' },
         // user: process.env.SPIDER_MONGODB_USER,
