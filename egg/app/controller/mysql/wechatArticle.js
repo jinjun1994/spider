2940020e65a7c419ffb444423ab01e3d7d9b5355
jinjun1994 <jinjun199403@163.com>
2020-05-24 20:35:53 +0800

    egg-文章内容搜索

--- egg/app/controller/mysql/wechatArticle.js
+++ egg/app/controller/mysql/wechatArticle.js
@@ -1,5 +1,6 @@
 'use strict';
 const Controller = require('egg').Controller;
+const { Op } = require('sequelize');
 /**
   * @controller
   */
@@ -7,10 +8,13 @@ class WechatArticlesController extends Controller {
 
   async index(ctx) {
 
-    const { account, biz } = ctx.query;
+    const { account, biz, content } = ctx.query;
     const options = {
       ...(account ? { account } : {}),
       ...(biz ? { biz } : {}),
+      ...(content ? { content_html: {
+        [Op.like]: `%${content}%`
+      } } : {}),
     };
     const articles = await ctx.service.mysql.wechatArticle.list(options);
     ctx.body = articles;
