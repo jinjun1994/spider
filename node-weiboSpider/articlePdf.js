dcbf7a4b63a10de5e8c16798ce9d4ba3c52e12a0
jinjun1994 <jinjun199403@163.com>
2020-05-24 10:38:54 +0800

    node-生成微信pdf

--- node-weiboSpider/articlePdf.js
+++ node-weiboSpider/articlePdf.js
@@ -1,11 +1,11 @@
 const PDFDocument = require("pdfkit");
 const fs = require("fs");
-const puppeteer = require("puppeteer");
-const filenamify = require('filenamify');
+const filenamify = require("filenamify");
 const api = require("./src/file.js");
-const PDFMerge = require('pdf-merge');
-
-let browser;
+const PDFMerge = require("pdf-merge");
+const { Cluster } = require("puppeteer-cluster");
+const PDFMerger = require("pdf-merger-js");
+const merge = require("easy-pdf-merge");
 
 (async function mkdir() {
   const { data: accounts } = await api.fetchAccount({
@@ -13,7 +13,31 @@ let browser;
     full: true,
   });
   console.log(accounts);
-  browser = await puppeteer.launch({ headless: true });
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: 8,
+    timeout: 300000,
+  });
+  await cluster.task(async ({ page, data: { url, biz, title } }) => {
+    console.log(biz, title);
+    await page.goto(url, { waitUntil: "networkidle0" });
+    await autoScroll(page);
+    console.log("2");
+    const pdf = await page.pdf({
+      path: `./articles/${biz}/${filenamify(title)}.pdf`,
+      format: "A4",
+      preferCSSPageSize: true,
+      // printBackground: true,
+      margin: {
+        top: 10,
+        right: 60,
+        bottom: 10,
+        left: 60,
+      },
+    });
+    console.log(3);
+  });
+  const mergeFiles = {};
   for (const account of accounts.rows) {
     const baseUrl = "./articles/" + account.biz;
     if (!fs.existsSync(baseUrl)) {
@@ -24,52 +48,59 @@ let browser;
       full: true,
       biz: account.biz,
     });
-    const articleFiles=[]
+    const articleFiles = [];
+    const newArticleFiles = [];
     for (const article of articles.rows) {
-        console.log(article.title);
-        articleFiles.push(baseUrl + `/${filenamify(article.title)}.pdf`)
-        console.log(filenamify(article.title));
-      console.log(account.biz);
+      articleFiles.push(baseUrl + `/${filenamify(article.title)}.pdf`);
       try {
         if (!fs.existsSync(baseUrl + `/${filenamify(article.title)}.pdf`)) {
-          await printPDF(article);
+          newArticleFiles.push(baseUrl + `/${filenamify(article.title)}.pdf`);
+          cluster.queue(article);
         }
       } catch (error) {
         console.log(error);
       }
     }
-    PDFMerge(articleFiles, {output: `${baseUrl}/${account.account?account.account:account.biz}.pdf`})
-.then((buffer) => {
-  console.log('done');
-});
-
-
-  
-   
+    mergeFiles[
+      `${baseUrl}/${
+        account.account ? account.account : account.biz
+      }公众号文章合集`
+    ] = {
+      all: articleFiles,
+      new: newArticleFiles,
+    };
   }
-  await browser.close();
+  await cluster.idle();
+  await cluster.close();
+  for (const [k, v] of Object.entries(mergeFiles)) {
+    if (fs.existsSync(`${k}.pdf`) ) {
+      fs.unlinkSync(`${k}.pdf`)
+    }
+      await mergePdfs(v.all, k);
+    
+  }
 })();
 
-async function printPDF({ url, biz, title } = {}) {
-  const page = await browser.newPage();
-  await page.goto(url, { waitUntil: "networkidle0" });
-  await autoScroll(page);
-  const pdf = await page.pdf({
-    path: `./articles/${biz}/${filenamify(title)}.pdf`,
-    format: "A4",
-    preferCSSPageSize: true,
-    // printBackground: true,
-    margin: {
-      top: 10,
-      right: 60,
-      bottom: 10,
-      left: 60,
-    },
-  });
+// async function printPDF({ page ,url, biz, title } = {}) {
 
-  await page.close();
-    return pdf
-}
+//   await page.goto(url, { waitUntil: "networkidle0" });
+//   await autoScroll(page);
+//   const pdf = await page.pdf({
+//     path: `./articles/${biz}/${filenamify(title)}.pdf`,
+//     format: "A4",
+//     preferCSSPageSize: true,
+//     // printBackground: true,
+//     margin: {
+//       top: 10,
+//       right: 60,
+//       bottom: 10,
+//       left: 60,
+//     },
+//   });
+
+//   // await page.close();
+//     // return pdf
+// }
 
 // 滚动到底部懒加载图片
 async function autoScroll(page) {
@@ -93,4 +124,28 @@ async function autoScroll(page) {
       }, 100);
     });
   });
+}
+
+async function mergePdfs(pdfs, name) {
+  try {
+    if (pdfs.length > 50) {
+      await PDFMerge(
+        [
+          ...pdfs.slice(0, 50),
+          ...(fs.existsSync(`${name}.pdf`) ? [`${name}.pdf`] : []),
+        ],
+        { output: `${name}.pdf` }
+      );
+      await mergePdfs(pdfs.slice(50), name);
+    } else {
+      await PDFMerge(
+        [...pdfs, ...(fs.existsSync(`${name}.pdf`) ? [`${name}.pdf`] : [])],
+        { output: `${name}.pdf` }
+      );
+    }
+
+    console.log(name);
+  } catch (error) {
+    console.log(error);
+  }
 }
